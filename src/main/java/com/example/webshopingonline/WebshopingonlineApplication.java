21d0a4a6b70b1a8c9ebf656435c1a50b72c94054
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-22 13:10:41 +0200

    init

---
+++ src/main/java/com/example/webshopingonline/WebshopingonlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopingonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopingonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopingonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webshopingonline/WebshopingonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopingonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopingonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
