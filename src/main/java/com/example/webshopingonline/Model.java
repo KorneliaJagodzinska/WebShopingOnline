f8c954253d3ec7a39f7a51d2d653c05f8f3d1921
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-23 21:06:42 +0200

    model

---
+++ src/main/java/com/example/webshopingonline/Model.java
@@ -0,0 +1,10 @@
+package com.example.webshopingonline;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Model {
+    public static void main(String[] args) {
+        List<String> names= Arrays.asList("Kasia", "basia", "zosia");
+    }
+}
